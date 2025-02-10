8cef36f0fc3abcb9e709a92fac857715e1d602d8
Mercy Fetzer <131705906+MercyFetzer7@users.noreply.github.com>
2025-02-10 15:22:18 -0700

    3:22 Feb 10th

--- Mission6_Fetzer/Models/Form.cs
+++ Mission6_Fetzer/Models/Form.cs
@@ -2,5 +2,21 @@ namespace Mission6.Models;
 
 public class Form
 {
+    public int FormId { get; set; }
     
+    public string Category { get; set; }
+    
+    public string Title { get; set; }
+    
+    public int Year { get; set; }
+    
+    public string Director { get; set; }
+    
+    public string Rating { get; set; }
+    
+    public bool Edited { get; set; }
+    
+    public string LentTo { get; set; }
+    
+    public string Notes { get; set; }
 }
--- Mission6_Fetzer/Models/MovieFormContext.cs
+++ Mission6_Fetzer/Models/MovieFormContext.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 namespace Mission6.Models;
 
-public class MovieFormContext
+public class MovieFormContext : DbContext
 {
+    public MovieFormContext(DbContextOptions<MovieFormContext> options) : base(options)
+    {
+    }
     
+    public DbSet<Form> Forms { get; set; }
 }
